98e9f7305cd58c6a69a24a29a60f13beb2a305b0
Kasun Dulara <kasundularaam@gmail.com>
2023-07-11 17:18:30 +0530

    PORT CHANGED

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
 //connect DB
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
 
 //routers
-const authRouter = require('./routes/user/auth')
-
+const authRouter = require("./routes/user/auth");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.use('/api/v1/auth',authRouter)
-
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
